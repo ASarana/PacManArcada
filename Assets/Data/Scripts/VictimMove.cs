956458da57bac74e657a7e59857ef612a480f34d
rolandxl <rolandxxxl@gmail.com>
2015-11-16 23:36:32 +0300

    стандартные библиотеки управления + движение
    Передвинул платформу, пакмана и камеру на координаты 100/0/100. Навесил
    на пакмана скрипт движения

---
+++ Assets/Data/Scripts/VictimMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictimMove : MonoBehaviour 
+{
+
+	private CharacterController victim; // Reference to the ball controller.
+	public Transform victim_trans;
+	public Transform hero;
+	public float speedvict; 
+	private Vector3 move;
+	
+
+	void Start () 
+	{
+		victim = GetComponent<CharacterController>();
+		victim_trans = GetComponent<Transform>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		move =  hero.position - victim_trans.position ;
+		move = move / move.magnitude;
+
+	}
+	void FixedUpdate () 
+	{
+
+		//victim.Move (move*0.5f);
+		victim.Move (move*speedvict);
+	}
+
+	void OnCollisionEnter() 
+	{
+		Debug.Log("Hit Something"); 
+		
+	}
+
+}
---
+++ Assets/Data/Scripts/pacman_control.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class pacman_control : MonoBehaviour 
+{
+	private CharacterController pacman; 
+	public Transform pacman_transform;
+	public float speed; 
+	private Vector3 move;
+	private Transform cam; 
+	private Vector3 camForward;
+	void Start () 
+	{
+		pacman = GetComponent<CharacterController>();
+		pacman_transform = GetComponent<Transform>();
+		cam = Camera.main.transform;
+	}
+	
+	void Update () 
+	{
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		float v = CrossPlatformInputManager.GetAxis("Vertical");
+		camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+		move = (v*camForward + h*cam.right).normalized;
+		pacman_transform.LookAt (new Vector3(h*1000,0,v*1000));
+	
+	}
+
+	private void FixedUpdate()
+	{
+			pacman.Move(speed*move);
+	}
+
+	void OnCollisionEnter() 
+	{
+
+
+	}
+}
