f307f29a953275c05c8fbc775533f61bf31dea39
rolandxl <rolandxxxl@gmail.com>
2015-11-18 20:58:39 +0300

    нормальное движение пакмана + взаимодействие
    пакман теперь двигается строго горизонтально либо вертикально +
    непрерывно, пока не упрется. Так же он определяет с какой стороны от
    него стена

--- Assets/Data/Scripts/pacman_control.cs
+++ Assets/Data/Scripts/pacman_control.cs
@@ -10,31 +10,56 @@ public class pacman_control : MonoBehaviour
 	private Vector3 move;
 	private Transform cam; 
 	private Vector3 camForward;
+	private Vector3 look;
+	private float v;
+	private float h;
+	private float v1;
+	private float h1;
+	public float colis_dist;
 	void Start () 
 	{
 		pacman = GetComponent<CharacterController>();
 		pacman_transform = GetComponent<Transform>();
 		cam = Camera.main.transform;
+		look = new Vector3 (100, 0, 0);
+		v = 0; 
+		h = 0;
+		v1 = 0;
+		h1 = 0;
 	}
 	
 	void Update () 
-	{
-		float h = CrossPlatformInputManager.GetAxis("Horizontal");
-		float v = CrossPlatformInputManager.GetAxis("Vertical");
+	{ 
+		if (v==0)	h = CrossPlatformInputManager.GetAxis("Horizontal");
+		if (h==0) 	v = CrossPlatformInputManager.GetAxis("Vertical");
+		
+		if (h > 0 && h > h1)  h1 = 1;
+		if (v > 0 && v > v1)  v1 = 1;
+		if (h < 0 && h < h1)	h1 = -1;
+		if (v < 0 && v < v1)	v1 = -1;
+		if (h == 0 && v != 0)	h1 = 0;
+		if (v == 0 && h != v)	v1 = 0;
+		
+		
 		camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
-		move = (v*camForward + h*cam.right).normalized;
-		pacman_transform.LookAt (new Vector3(h*1000,0,v*1000));
-	
+		move = (v1*camForward + h1*cam.right).normalized;
+		if (h == h1 && v == v1 && h == 0 && v == 0 && h1 == 0 && v1 == 0)
+			look.Set (100, 0, 0);
+		else look = (v1 * camForward + h1 * cam.right) * 1000;
+		
+		if (Physics.Raycast (pacman_transform.position, pacman_transform.forward, colis_dist))
+			print ("forward");
+		if (Physics.Raycast (pacman_transform.position, -pacman_transform.forward, colis_dist))
+			print ("back");
+		if (Physics.Raycast (pacman_transform.position, pacman_transform.right, colis_dist))
+			print ("right");
+		if (Physics.Raycast (pacman_transform.position, -pacman_transform.right, colis_dist))
+			print ("left");
 	}
-
+	
 	private void FixedUpdate()
 	{
-			pacman.Move(speed*move);
-	}
-
-	void OnCollisionEnter() 
-	{
-
-
+		pacman.Move(speed*move);
+		pacman_transform.LookAt (look);
 	}
 }
