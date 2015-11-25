e78654ae1d9c683ba80f336ce82a8ba85c79f62e
rolandxl <rolandxxxl@gmail.com>
2015-11-25 19:16:07 +0300

    Текстуры + движение
    Каждое приведение теперь уникально. Если скушать красную очку, то они на
    5 секунд развенутся и убегут в правый угол

--- Assets/Data/Scripts/MoveNav.cs
+++ Assets/Data/Scripts/MoveNav.cs
@@ -6,18 +6,27 @@ public class MoveNav : MonoBehaviour
 	public Transform Goal; //куда вигаться, сюда впихнем пакмана
 	private NavMeshAgent agent; //текущий объект
 	private Transform ghost_transform; //текущий объект
+	private Vector3 home;
 
 	// Use this for initialization
 	void Start () 
 	{
 		agent = GetComponent<NavMeshAgent>();
 		ghost_transform = GetComponent<Transform> ();
+		home = new Vector3 (56, 0, 56);
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		agent.destination = Goal.position; //направляем к цели - пакману
+		if (ghost_transform.transform.rotation.y != 0) {
+			agent.destination = Goal.position; //направляем к цели - пакману
+		} 
+		else 
+		{
+			agent.destination = home;
+		}
 
 		if(agent.remainingDistance < 2) //если догнал, пока ничего... но лучше взрыв
 		{
--- Assets/Data/Scripts/redpoint.cs
+++ Assets/Data/Scripts/redpoint.cs
@@ -5,18 +5,23 @@ public class redpoint : MonoBehaviour
 {
 	
 	public Transform pacman_trans; //сюда подставим пакмана
-	public Transform ghost;
+	public Transform clyde;
+	public Transform pinky;
+	public Transform inky;
+	public Transform blinky;
 	private Transform point_trans; //это для манипуляций с точкой
+	private float timer;
 	public int pointcount; //это чтобы знать что уровень закончен количество съеденных точек
 	// Use this for initialization
 	void Start () 
 	{
 		point_trans = GetComponent<Transform> (); // берем трансформ текущего объекта - точки
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
+	{ 
 		//отследим, что пакман дотронулся до точки
 		if (point_trans.position.x <= (pacman_trans.position.x + 0.5) && point_trans.position.x >= (pacman_trans.position.x - 0.5)
 		    && point_trans.position.z <= (pacman_trans.position.z + 0.5) && point_trans.position.z >= (pacman_trans.position.z - 0.5) 
@@ -27,7 +32,21 @@ public class redpoint : MonoBehaviour
 			print ("Держитесь, суки");
 			point_trans.position= new Vector3(point_trans.position.x,point_trans.position.y-10,point_trans.position.z);
 			pointcount++;
-			ghost.LookAt(new Vector3(0,0,1000));
+			clyde.rotation = new Quaternion(0,0,180,0);
+			pinky.rotation = new Quaternion(0,0,180,0);
+			inky.rotation = new Quaternion(0,0,180,0);
+			blinky.rotation = new Quaternion(0,0,180,0);
+			timer = Time.time;
 		}
+
+		print (Time.time);
+		if (Time.time - timer >= 5) 
+		{
+			clyde.rotation = new Quaternion(0,0,0,0);
+			pinky.rotation = new Quaternion(0,0,0,0);
+			inky.rotation = new Quaternion(0,0,0,0);
+			blinky.rotation = new Quaternion(0,0,0,0);
+		}
+
 	}
 }
